3e6dab021e9ee487b37973017b7031a18af72783
Trini Morrison <TRPMorrison@gmail.com>
2015-05-26 06:48:21 -0400

    REVISED JS

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
 
     var listItems = $('#listing');
-         //console.log(listing.title);
-var listingItem = $('#item');
+    //console.log(listing.title);
+    // var listingItem = $('#item');
 
     var itemTitle,
-    itemImage,
-    itemPrice,
-    itemCurrency,
-    listingItem;
+        itemImage,
+        itemPrice,
+        itemCurrency,
+        listingItem;
 
     etsyItems.forEach(function(item) {
-      itemTitle = '<p>' + item.title + '</p>';
-      itemImage = '<img src="' + item.url + '"/>';
-      itemPrice='<p>' + item.price + '</p>';
-      itemCurrency='<p>' + item.currency_code + '</p>';
+        itemTitle = '<p>' + item.title + '</p>';
+        itemImage = '<img src="' + item.url + '"/>';
+        itemPrice = '<p>' + item.price + '</p>';
+        itemCurrency = '<p>' + item.currency_code + '</p>';
 
-      listingItem = '<li>' + itemTitle + itemImage + "$" + itemPrice + itemCurrency + '</li>';
+        listingItem = '<li>' + itemTitle + itemImage + "$" + itemPrice + itemCurrency + '</li>';
 
-        listItems.append(listingItem);
+        listItems.append('#item');
     });
 });
