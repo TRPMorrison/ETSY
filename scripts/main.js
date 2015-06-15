e23f99a0f18f3d28ad6432ce84e80b086e62e284
Trini Morrison <TRPMorrison@gmail.com>
2015-06-15 17:13:30 -0400

    STYLINGS

--- scripts/main.js
+++ scripts/main.js
@@ -11,16 +11,15 @@ $(document).ready(function() {
 
     listings.results.forEach(function(item) {
         urlImage = item.Images[0].url_170x135;
+itemImage = '<a class="resultsImage"><img src="' + urlImage + '" /></a>';
 
         itemTitle = '<p>' + item.title + '</p>';
 
-        itemImage = '<a class="resultsImage"><img src="' + urlImage + '" /></a>';
+     itemCurrency = '<span>' + item.currency_code + '</span>';
+        itemPrice = '<span>' + item.price + '</span>';
 
-        itemPrice = '<p>' + item.price + '</p>';
 
-        itemCurrency = '<p>' + item.currency_code + '</p>';
-
-        listingItems = '<li>' + itemTitle + itemImage + "$" + itemPrice + itemCurrency + '</li>';
+        listingItems = '<li>' +  itemImage + itemTitle + "$" + itemPrice + itemCurrency + '</li>';
 
         listItems.append(listingItems);
     });
